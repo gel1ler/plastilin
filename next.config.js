140ad45f496a8e45bf7d2993f7a609394f8582cb
gel1ler <alex00713571@gmail.com>
2023-11-20 18:25:31 +0300

    1.0

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: 'https://plastilin-deti.ru/',
-    trailingSlash: true,
-    images: {
-        unoptimized: true,
-    }
+    // output: 'export',
+    // assetPrefix: 'https://plastilin-deti.ru/',
+    // trailingSlash: true,
+    // images: {
+    //     unoptimized: true,
+    // }
 }
 
 
