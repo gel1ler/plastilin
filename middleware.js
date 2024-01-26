2acaafdd58853e8e7eba80ac8c7ffcf827adb74d
gel1ler <alex00713571@gmail.com>
2024-01-26 04:40:14 +0300

    Cnahges

--- middleware.js
+++
@@ -1,8 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs"
-export default authMiddleware({
-  publicRoutes: req => !req.url.includes('/admin')
-})
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
