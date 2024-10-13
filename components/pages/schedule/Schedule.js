16b1ea9f2de9e6792376afc8c7602f2b41df6408
gel1ler <abanichev16@gmail.com>
2024-10-13 14:35:43 +0300

    Suspense error fixed

--- components/pages/schedule/Schedule.js
+++ components/pages/schedule/Schedule.js
@@ -8,7 +8,8 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css'
 import Image from 'next/image'
 import { useSearchParams } from 'next/navigation';
-import { smallGroupLessons, bigGroupLessons } from '@/data';
+import { smallGroupLessons, bigGroupLessons } from '@/data'
+import { Suspense } from 'react'
 
 const days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ']
 
@@ -62,106 +63,108 @@ const Schedule = () => {
     };
 
     return (
-        <Box sx={{ py: 2, width: '100vw' }}>
-            <Typography textAlign='center' variant='h4' mt={10}>
-                <b>Расписание {name}</b>
-            </Typography>
-            <Typography textAlign='center' variant='body2'>
-                Листайте или нажимайте на стрелки
-            </Typography>
-            <Box
-                sx={{
-                    my: 4,
-                    minHeight: '50vh',
-                    width: '100vw',
-                    overflowX: 'hidden'
-                }}
-            >
-                <Box sx={{ mb: 4, position: 'relative', width: '100vw' }}>
-                    <Slider ref={sliderRef} {...settings}>
-                        {days.map((day, key) => (
-                            <Box key={key} sx={{ textAlign: 'center' }}>
+        <Suspense>
+            <Box sx={{ py: 2, width: '100vw' }}>
+                <Typography textAlign='center' variant='h4' mt={10}>
+                    <b>Расписание {name}</b>
+                </Typography>
+                <Typography textAlign='center' variant='body2'>
+                    Листайте или нажимайте на стрелки
+                </Typography>
+                <Box
+                    sx={{
+                        my: 4,
+                        minHeight: '50vh',
+                        width: '100vw',
+                        overflowX: 'hidden'
+                    }}
+                >
+                    <Box sx={{ mb: 4, position: 'relative', width: '100vw' }}>
+                        <Slider ref={sliderRef} {...settings}>
+                            {days.map((day, key) => (
+                                <Box key={key} sx={{ textAlign: 'center' }}>
 
-                                {/* Controls */}
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        gap: 3
-                                    }}
-                                >
-                                    <Image
-                                        src='/icons/arrow_black.svg'
-                                        width={25}
-                                        height={25}
-                                        alt='prev'
-                                        style={{
-                                            transform: 'rotate(180deg)',
-                                            cursor: 'pointer'
-                                        }}
-                                        onClick={() => sliderRef.current.slickPrev()}
-                                    />
-                                    <Box sx={{ textAlign: 'center' }}>
-                                        <Typography variant='h6' fontWeight='bold'>
-                                            {day}
-                                        </Typography>
-                                        <Typography color='GrayText'>
-                                            {week[key]}
-                                        </Typography>
-                                    </Box>
-                                    <Image
-                                        src='/icons/arrow_black.svg'
-                                        width={25}
-                                        height={25}
-                                        alt='prev'
-                                        style={{
-                                            cursor: 'pointer'
-                                        }}
-                                        onClick={() => sliderRef.current.slickNext()}
-                                    />
-                                </Box>
-
-                                {/* Content */}
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        alignItems: 'center',
-                                        // height: '60vh',
-                                        // overflowY: 'auto',
-                                        // overflowX: 'hidden'
-                                    }}
-                                >
+                                    {/* Controls */}
                                     <Box
                                         sx={{
-                                            bgcolor: 'white',
-                                            width: '80%',
-                                            display: 'grid',
-                                            gridTemplateColumns: '1fr',
-                                            mx: 2,
-                                            gap: '10px',
-                                            p: 2,
-                                            boxShadow: '0 0px 5px 2px rgba(0, 0, 0, 0.1)',
-                                            borderRadius: '20px',
-                                            maxWidth: '800px',
-                                            my: 1
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            gap: 3
                                         }}
                                     >
-                                        {groups[validGroup].map((lesson, lessonIndex) => (
-                                            <React.Fragment key={lessonIndex}>
-                                                <Item props={lesson} index={key} />
-                                                {lessonIndex === groups[validGroup][key].length - 1 ? null : <Divider />}
-                                            </React.Fragment>
-                                        ))}
+                                        <Image
+                                            src='/icons/arrow_black.svg'
+                                            width={25}
+                                            height={25}
+                                            alt='prev'
+                                            style={{
+                                                transform: 'rotate(180deg)',
+                                                cursor: 'pointer'
+                                            }}
+                                            onClick={() => sliderRef.current.slickPrev()}
+                                        />
+                                        <Box sx={{ textAlign: 'center' }}>
+                                            <Typography variant='h6' fontWeight='bold'>
+                                                {day}
+                                            </Typography>
+                                            <Typography color='GrayText'>
+                                                {week[key]}
+                                            </Typography>
+                                        </Box>
+                                        <Image
+                                            src='/icons/arrow_black.svg'
+                                            width={25}
+                                            height={25}
+                                            alt='prev'
+                                            style={{
+                                                cursor: 'pointer'
+                                            }}
+                                            onClick={() => sliderRef.current.slickNext()}
+                                        />
+                                    </Box>
+
+                                    {/* Content */}
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'center',
+                                            // height: '60vh',
+                                            // overflowY: 'auto',
+                                            // overflowX: 'hidden'
+                                        }}
+                                    >
+                                        <Box
+                                            sx={{
+                                                bgcolor: 'white',
+                                                width: '80%',
+                                                display: 'grid',
+                                                gridTemplateColumns: '1fr',
+                                                mx: 2,
+                                                gap: '10px',
+                                                p: 2,
+                                                boxShadow: '0 0px 5px 2px rgba(0, 0, 0, 0.1)',
+                                                borderRadius: '20px',
+                                                maxWidth: '800px',
+                                                my: 1
+                                            }}
+                                        >
+                                            {groups[validGroup].map((lesson, lessonIndex) => (
+                                                <React.Fragment key={lessonIndex}>
+                                                    <Item props={lesson} index={key} />
+                                                    {lessonIndex === groups[validGroup][key].length - 1 ? null : <Divider />}
+                                                </React.Fragment>
+                                            ))}
+                                        </Box>
                                     </Box>
                                 </Box>
-                            </Box>
-                        ))}
-                    </Slider>
+                            ))}
+                        </Slider>
+                    </Box >
                 </Box >
             </Box >
-        </Box >
+        </Suspense>
     );
 };
 
